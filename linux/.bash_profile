9a143b9cb4144eecd86cb69fc506a4e30f520b08
hasansino <4024450+hasansino@users.noreply.github.com>
2025-03-31 20:20:55 +0300

    do not forget about comments

--- linux/.bash_profile
+++ linux/.bash_profile
@@ -2,10 +2,16 @@
 # linux profile
 # executed once per login session
 
-if [ -f ~/.profile ]; then
-    . ~/.profile
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+    fi
 fi
 
-if [ -f ~/.bashrc ]; then
-    . ~/.bashrc
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
 fi
--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ else
 fi
 
 update_file() {
-    local header="╔════════════════════ [ UNIXENV CONFIG START ] ════════════════════╗"
-    local footer="╚════════════════════ [ UNIXENV CONFIG END   ] ════════════════════╝"
+    local header="# ╔════════════════════ [ UNIXENV CONFIG START ] ════════════════════╗"
+    local footer="# ╚════════════════════ [ UNIXENV CONFIG END   ] ════════════════════╝"
     local source="$1"
     local target="$2"
 
